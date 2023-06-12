a1218743e512f9b9083bbbb05ca9f45d3ef50ea4
zizo2k21 <aziz@LAPTOP-QGRKF93V>
2023-06-12 14:56:43 +0200

    write small functions

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-//types
+// Types
 type Operator = "+" | "-" | "*" | "/" | "MOD";
 type UnaryOperator = "NEGATE";
 
-//utils
+// Vérifie si le token est un opérateur binaire valide
 function isOperator(token: string): token is Operator {
   return token === "+" || token === "-" || token === "*" || token === "/" || token === "MOD";
 }
 
+// Vérifie si le token est un opérateur unaire valide
 function isUnaryOperator(token: string): token is UnaryOperator {
   return token === "NEGATE";
 }
 
+// Effectue l'opération binaire correspondant à l'opérateur
 function performBinaryOperation(operator: Operator, operand1: number, operand2: number): number {
-if (operator === "/" && operand2 === 0) {
+  if (operator === "/" && operand2 === 0) {
     throw new Error("Division par zéro impossible");
-    }
+  }
   switch (operator) {
     case "+":
       return operand1 + operand2;
@@ -27,67 +29,76 @@ if (operator === "/" && operand2 === 0) {
     case "MOD":
       return operand1 % operand2;
     default:
-      throw new Error(`operateur binaire inconnu: ${operator}`);
+      throw new Error(`opérateur binaire inconnu : ${operator}`);
   }
 }
 
+// Effectue l'opération unaire correspondant à l'opérateur
 function performUnaryOperation(operator: UnaryOperator, operand: number): number {
   switch (operator) {
     case "NEGATE":
       return -operand;
     default:
-      throw new Error(`operateur unaire iconnu: ${operator}`);
+      throw new Error(`opérateur unaire inconnu : ${operator}`);
   }
 }
 
-//domain
-
+// Fonction principale RPN
 function rpn(expression: string): number {
-    // On sépare les éléments de l'expression en utilisant l'espace comme séparateur
-    const tokens = expression.trim().split(" ");
-    const stack: number[] = [];
-  
-    for (const token of tokens) {
-      if (isOperator(token)) {
-        // On récupère les deux opérandes du sommet de la pile
-        const operand2 = stack.pop();
-        const operand1 = stack.pop();
-        if (operand1 === undefined || operand2 === undefined) {
-            // On lance une erreur si le nombre d'opérandes est insuffisant
-          throw new Error("Expression RPN invalide : opérandes insuffisants");
-        }
-        // On effectue l'opération binaire avec l'opérateur et les opérandes récupérés
-        const result = performBinaryOperation(token, operand1, operand2);
-        // On empile le résultat obtenu
-        stack.push(result);
-      } else if (isUnaryOperator(token)) {
-        // On récupère l'opérande du sommet de la pile
-        const operand = stack.pop();
-        if (operand === undefined) {
-            // On lance une erreur si l'opérande est manquant
-          throw new Error("Expression RPN invalide : opérandes insuffisants");
-        }
-        // On effectue l'opération unaire avec l'opérateur et l'opérande récupérés
-        const result = performUnaryOperation(token, operand);
-        // On empile le résultat obtenu
-        stack.push(result);
-      } else {
-        // Si le token est un nombre, on le convertit en flottant et on l'empile
-        const operand = parseFloat(token);
-        if (isNaN(operand)) {
-            // On lance une erreur si le token n'est pas un nombre valide
-          throw new Error(`token invalide: ${token}`);
-        }
-        stack.push(operand);
-      }
+  const tokens = expression.trim().split(" ");
+  const stack: number[] = [];
+
+  for (const token of tokens) {
+    if (isOperator(token)) {
+      performBinaryOperationOnStack(stack, token);
+    } else if (isUnaryOperator(token)) {
+      performUnaryOperationOnStack(stack, token);
+    } else {
+      pushOperandToStack(stack, token);
     }
-  
-    if (stack.length !== 1) {
-        // On lance une erreur s'il reste un nombre incorrect d'opérandes dans la pile
-      throw new Error("Expression RPN invalide : trop d'opérandes");
-    }
-    // On retourne le résultat final qui se trouve au sommet de la pile
-    return stack[0];
   }
 
-  export default rpn;
+  if (stack.length !== 1) {
+    throw new Error("Expression RPN invalide : trop d'opérandes");
+  }
+
+  return stack[0];
+}
+
+// Effectue l'opération binaire avec l'opérateur et les opérandes récupérés et empile le résultat
+function performBinaryOperationOnStack(stack: number[], operator: Operator): void {
+  const operand2 = stack.pop();
+  const operand1 = stack.pop();
+
+  if (operand1 === undefined || operand2 === undefined) {
+    throw new Error("Expression RPN invalide : opérandes insuffisants");
+  }
+
+  const result = performBinaryOperation(operator, operand1, operand2);
+  stack.push(result);
+}
+
+// Effectue l'opération unaire avec l'opérateur et l'opérande récupérés et empile le résultat
+function performUnaryOperationOnStack(stack: number[], operator: UnaryOperator): void {
+  const operand = stack.pop();
+
+  if (operand === undefined) {
+    throw new Error("Expression RPN invalide : opérandes insuffisants");
+  }
+
+  const result = performUnaryOperation(operator, operand);
+  stack.push(result);
+}
+
+// Convertit le token en flottant et l'empile sur la pile
+function pushOperandToStack(stack: number[], token: string): void {
+  const operand = parseFloat(token);
+
+  if (isNaN(operand)) {
+    throw new Error(`token invalide : ${token}`);
+  }
+
+  stack.push(operand);
+}
+
+export default rpn;
